d428a93f8924bfffa08d6402d93ae83355381b45
ilana0905 <164385122+ilana0905@users.noreply.github.com>
2024-03-21 17:09:45 +0000

    Update physics model.py

--- physics model.py
+++ physics model.py
@@ -1,10 +1,11 @@
 import csv
-with open('DATA FOR MODEL.csv', newline='') as csvfile:
+with open('DATA FOR MODEL.csv', newline='') as csvfile: #Importing our csv of data for model of power consumption
     reader = csv.reader(csvfile, delimiter=',')
     pcd = list(reader)
+
+pcd = [[x[0], float(x[1])] for x in pcd[1:]]#Removing the headings of the csv file
+print(pcd)
 
 #MODEL OF POWER CONSUMPTION
 def powerconsumption(month,time):
   return #whatever we decide the graph to be
-print(pcd)
-pcd = [[x[0], float(x[1])] for x in pcd[1:]]
