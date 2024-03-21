29d3a363864f62e84f5827230e1bb267e8fa8043
ilana0905 <164385122+ilana0905@users.noreply.github.com>
2024-03-21 18:23:49 +0000

    Update physics model.py

--- physics model.py
+++ physics model.py
@@ -1,15 +1,29 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
+
 with open('DATA FOR MODEL.csv', newline='') as csvfile: #Importing our csv of data for model of power consumption
     reader = csv.reader(csvfile, delimiter=',')
     pcd = list(reader)
 
-proportionsforhours = [[x[0], (x[1])] for x in pcd[1:]]#Removing the headings off the columns 'Proportion of daily energy consumption consumed in the given hour'
+proportionsforhours = [[x[0], (x[1])] for x in pcd[1:26]]
 
-energyperhour = [[x[3], (x[4])] for x in pcd[1:]]#Removing the headings off the columns 'Average energy consumption per hour (in kWh)'
+energyperday = [[x[3], (x[4])] for x in pcd[1:14]]
 
 #MODEL OF POWER CONSUMPTION
-def powerconsumption(month,time):
-    x=month
-    y=time
-    return proportionsforhours[x][1]
+def model(month,time):
+    x=month-1 #Subtracting 1 to factor in for first element being 0th index but 1st month (January), for example
+    y=time-1
+    datatoplot=np.zeros((5,1))
+    datatoplot[0,0]=-float(proportionsforhours[x][1])*float(energyperday[y][1])
+    sources=['Energy Demand','Tidal Supply', 'Solar Supply', 'Onshore Wind Supply','Net Energy']
+    print(datatoplot)
+    plt.bar(sources, datatoplot[0:,0])
+    plt.xlabel('Resource')
+    plt.ylabel('Energy Demand/Supply in kWh per hour')
+    plt.title('dkvks')
+    plt.show()
+
+powerconsumption(2,5)
+#{proportionsforhours[y][0]} o'clock on a given day in {energyperday[x][0]}
+#monthandtime=str(energyperday[x][0] + proportionsforhours[y][0])
