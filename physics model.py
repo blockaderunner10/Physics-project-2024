d06a43fd7217d59d4e6b836fc9f5af3a06ab79fb
ilana0905 <164385122+ilana0905@users.noreply.github.com>
2024-03-21 17:02:21 +0000

    Update physics model.py

--- physics model.py
+++ physics model.py
@@ -1,5 +1,8 @@
 print ("test")
 import csv
+with open('DATAFORMODEL.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    pcd = list(reader)
 
 #MODEL OF POWER CONSUMPTION
 def powerconsumption(month,time):
