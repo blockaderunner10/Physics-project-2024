c501bd3dcf6461c844f69a7885ab053786276d9a
ilana0905 <164385122+ilana0905@users.noreply.github.com>
2024-03-21 17:08:23 +0000

    Update physics model.py

--- physics model.py
+++ physics model.py
@@ -7,3 +7,4 @@ with open('DATA FOR MODEL.csv', newline='') as csvfile:
 def powerconsumption(month,time):
   return #whatever we decide the graph to be
 print(pcd)
+pcd = [[x[0], float(x[1])] for x in pcd[1:]]
