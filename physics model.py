0429fbd53403cf20823b254ff295f144358caf7a
ilana0905 <164385122+ilana0905@users.noreply.github.com>
2024-03-21 18:44:37 +0000

    Update physics model.py

--- physics model.py
+++ physics model.py
@@ -12,23 +12,24 @@ proportionsforhours = [[x[0], (x[1])] for x in pcd[1:26]]#Removing headers and g
 energyperday = [[x[3], (x[4])] for x in pcd[1:14]]
 
 #MODEL OF POWER CONSUMPTION
-def model(month,time):
-    y=month-1 #Subtracting 1 to factor in for first element being 0th index in the array but 1st month (January), for example
-    x=time-1
+def model(month,time,noofSP,noofWT,noofTT):#Inputs will be month, time, number of Solar Panels, number of Wind Turbines, number of Tidal Turbines
+    month=month-1 #Subtracting 1 to factor in for first element being 0th index in the array but 1st month (January), for example
+    time=time-1 
     
     sources=['Energy Demand','Tidal Supply', 'Solar Supply', 'Onshore Wind Supply','Net Energy']#Creating categories for x axis bar charts
     
     datatoplot=np.zeros((5,1)) #Creating an empty array for values to plot
     
     #Calculating Energy Demand per hour from data in csv
-    datatoplot[0,0]=-float(proportionsforhours[x][1])*float(energyperday[y][1])
+    datatoplot[0,0]=-float(proportionsforhours[time][1])*float(energyperday[month][1])
 
     print(datatoplot)
     
+    #Plotting bar chart
     plt.bar(sources, datatoplot[0:,0])
     plt.xlabel('Resource')
     plt.ylabel('Energy Demand/Supply in kWh per hour')
-    plt.title(f"Energy Demand against our plan for renewable energy at {proportionsforhours[x][0]}:00 in {energyperday[y][0]}")
+    plt.title(f"Energy Demand against our plan for renewable energy at {proportionsforhours[time][0]}:00 in {energyperday[month][0]}")
     plt.show()
 
-model(2,21)#Testing with February 21:00
+model(2,12,0,0,0)#Testing with February 21:00
