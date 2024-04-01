dbc9b16946781c22df1fe6ef3434b9e51ce07dc7
blockaderunner10 <118437326+blockaderunner10@users.noreply.github.com>
2024-04-01 16:46:07 +0100

    Update physics model.py
    added to data dictionary

--- physics model.py
+++ physics model.py
@@ -69,7 +69,8 @@ def getDateInt(desiredmonth):
     monthDict = {"JAN":1, "FEB":2, "MAR":3, "APR":4, "MAY":5, "JUN":6, "JUL":7,
                  "AUG":8, "SEP":9, "OCT":10, "NOV":11, "DEC":12, "jan":1, "feb":2,
                  "mar":3, "apr":4,"may":5,"jun":6,"jul":7,"aug":8,"sep":9,"oct":10,
-                 "nov":11,"dec":12}
+                 "nov":11,"dec":12, "Jan":1, "Feb":2,"Mar":3, "Apr":4,"May":5,"Jun":6,
+                 "Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12, }
     if desiredmonth not in monthDict:
         while True:
             desiredmonth = input("Please re-enter the desired month in a 3 letter form (CAPS or no caps are allowed)")
