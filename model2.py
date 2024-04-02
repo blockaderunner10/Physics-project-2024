639d09db6b338d4d1529cb1910d329538fa71208
ilana0905 <164385122+ilana0905@users.noreply.github.com>
2024-04-02 14:39:50 +0100

    Update model2.py

--- model2.py
+++ model2.py
@@ -19,7 +19,7 @@ energyperday = [[x[3], (x[4])] for x in dfm[1:13]]
 windturb = [[(x[5]), (x[6])] for x in wtd[1:13]]
 solardata = [[(x[4]),(x[5]),(x[6])] for x in sd[1:13]]
 daysinmonth= [(1,31), (2,28), (3,31), (4,30), (5,31), (6,30), (7,31),(8,31), (9,30), (10,31), (11,30), (12,31)]
-months=['January','Febuary','March','April','May','June','July','August','September','October','November','December']
+months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
 def model():
     energypermonth=np.zeros((12,1))
@@ -64,7 +64,7 @@ def windcalc(desiredwindturb,desireddiameter):
         plt.bar(months[i], windenergykWhpermonth[i],color='green')
     plt.xlabel('Months')
     plt.ylabel('Energy produced by wind turbines (kWh)')
-    plt.title(f"Energy produced by {int(desiredwindturb)} wind turbines per month")
+    plt.title(f"Energy produced by {int(desiredwindturb)} ({desireddiameter}m) wind turbines per month")
     plt.tick_params(axis='both', which='major', labelsize=6)
     plt.show()
     return(windenergykWhpermonth)
@@ -82,7 +82,7 @@ def solarcalcs(desiredarea,desiredsolarpanels):
         plt.bar(months[j],monthlysolarenergyinkWh[j][0],color='green')
     plt.xlabel('Months')
     plt.ylabel('Energy produced by solar panels (kWh)')
-    plt.title(f"Energy produced by {int(desiredsolarpanels)} solar panels per month")
+    plt.title(f"Energy produced by {int(desiredsolarpanels)} ({desiredarea}m^2) solar panels per month")
     plt.tick_params(axis='both', which='major', labelsize=6)
     plt.show()
     return monthlysolarenergyinkWh
@@ -102,9 +102,25 @@ def netenergy(energypermonth,windenergykWhpermonth,monthlysolarenergyinkWh):
     plt.show()
     return monthlynetenergyinkWh
 
+def storage(monthlynetenergyinkWh):
+    storageinkWh=np.zeros((12,1))
+    for i in range (12):
+        storageinkWh[i][0]=storageinkWh[i-1][0]+monthlynetenergyinkWh[i][0]
+        if storageinkWh[i]>0:
+            plt.bar(months[i],storageinkWh[i][0],color='green')
+        else:
+            plt.bar(months[i],storageinkWh[i][0],color='red')
+    plt.xlabel('Months')
+    plt.ylabel('Energy in Storage at the start each month (in kWh)')
+    plt.title(f"Energy in Storage at the start each month with {int(desiredsolarpanels)} ({desiredarea}m^2) solar panels and {int(desiredwindturb)} ({desireddiameter}m blade) wind turbines")
+    plt.tick_params(axis='both', which='major', labelsize=6)
+    plt.show()
+    return storageinkWh
+
 desiredwindturb,desireddiameter=getinfowind()
 desiredsolarpanels,desiredarea=getNoOfSolarPanels()
 windenergykWhpermonth=windcalc(desiredwindturb,desireddiameter)
 monthlysolarenergyinkWh=solarcalcs(desiredarea,desiredsolarpanels)
 energypermonth=model()
 monthlynetenergyinkWh=netenergy(energypermonth,windenergykWhpermonth,monthlysolarenergyinkWh)
+storageinkWh=storage(monthlynetenergyinkWh)
